18ae99730a407d102bed7c6aa8e68363d926ecbf
Yotlan <momo54@gmail.com>
2023-05-26 13:19:59 +0200

    Fix and test decompose algorithm from lusail paper

--- py-lusail.py
+++ py-lusail.py
@@ -2,6 +2,8 @@ import rdflib
 import click
 from SPARQLWrapper import SPARQLWrapper, JSON
 import sys
+import re
+import json
 
 class Node:
     def __init__(self, value: str):
@@ -22,6 +24,9 @@ class Edge:
         self.predicate = predicate
         self.object = object
 
+    def __repr__(self) -> str:
+        return self.subject.value + " " + self.predicate + " " + self.object.value
+
     def __str__(self) -> str:
         return self.subject.value + " " + self.predicate + " " + self.object.value
 
@@ -35,9 +40,10 @@ class QueryTree:
         nodes = set()
         edges = set()
 
+        prefix = set()
+
         for line in query_txt.splitlines(keepends=True):
             if line.strip().startswith("#") or \
-                line.strip().startswith("PREFIX") or \
                 line.strip().startswith("SELECT") or \
                 line.strip().startswith("WHERE") or \
                 line.strip().startswith("{") or \
@@ -49,7 +55,11 @@ class QueryTree:
                 line.strip() == '':
                 continue
 
-            triples.add(line.strip())
+            elif line.strip().startswith("PREFIX"):
+                prefix.add(line.strip())
+
+            else:
+                triples.add(line.strip())
 
         for triple in triples:
             sep_triple = str(triple).split()
@@ -79,22 +89,29 @@ class QueryTree:
 
         self.nodes = nodes
         self.edges = edges
+        self.prefix = prefix
     
     def __repr__(self) -> str:
         return self.query
+
+    def __str__(self) -> str:
+        return self.query
     
-    def get_edges(self, vrtx: str) -> set:
+    def get_edges(self, vrtx: Node) -> set:
         edges = set()
         for edge in self.edges:
-            if str(edge.subject) == vrtx or str(edge.object) == vrtx:
+            #print(f"vrtx: {vrtx} -> subject: {edge.subject} -> object: {edge.object}")
+            if str(edge.subject) == str(vrtx) or str(edge.object) == str(vrtx):
                 edges.add(edge)
+        #print(f"[{len(self.edges)}] - vrtx: {vrtx} -> edges: {edges}")
         return edges
     
     def get_triple_patterns(self) -> set:
         return self.edges
     
     def add_edge(self, edge: Edge):
-        self.query.replace(".\n}", str(edge))
+        self.query = re.sub("\.\n}", ".\n" + str(edge) + " .\n}", self.query)
+        #print(self.query)
         self.edges.add(edge)
         self.nodes.add(edge.subject)
         self.nodes.add(edge.object)
@@ -102,8 +119,9 @@ class QueryTree:
     
     def get_relevant_source(self, endpoint: SPARQLWrapper) -> JSON:
         query = self.query
-        query.replace("WHERE {", "WHERE { GRAPH ?g {")
-        query.replace(".\n}", ". }\n }")
+        query = re.sub("SELECT \*", "SELECT DISTINCT ?g", query)
+        query = re.sub("WHERE {", "WHERE { GRAPH ?g {", query)
+        query = re.sub(".\n}", ". }\n }", query)
         endpoint.setQuery(query)
         return endpoint.query().convert()
     
@@ -114,18 +132,22 @@ class QueryTree:
                 nodes.add(node)
         return nodes
 
-def create_subquery(edge: Edge) -> QueryTree:
-    return QueryTree("SELECT * \nWHERE {\n " + str(edge.subject) + " " + edge.predicate + " " + str(edge.object) + " .\n}")
+def create_subquery(edge: Edge, prefixs: set) -> QueryTree:
+    str_prefix = ""
+    for prefix in prefixs:
+        str_prefix+= str(prefix) + "\n"
+    return QueryTree(str_prefix + "SELECT * \nWHERE {\n " + str(edge.subject) + " " + edge.predicate + " " + str(edge.object) + " .\n}")
 
-def get_parent_subquery(vrtx: str, subqueries: set) -> QueryTree:
+def get_parent_subquery(vrtx: Node, subqueries: set) -> QueryTree:
     for subquery in subqueries:
         for edge in subquery.get_edges(vrtx):
-            if str(edge.object) == vrtx:
+            if str(edge.subject) == str(vrtx) or str(edge.object) == str(vrtx):
                 return subquery
             
-def can_be_added_to_subquery(subquery: QueryTree, edge: Edge, endpoint: SPARQLWrapper) -> bool:
+def can_be_added_to_subquery(subquery: QueryTree, edge: Edge, V: set, endpoint: SPARQLWrapper) -> bool:
+    #print(subquery)
     results_1 = subquery.get_relevant_source(endpoint)
-    results_2 = create_subquery(edge).get_relevant_source(endpoint)
+    results_2 = create_subquery(edge, subquery.prefix).get_relevant_source(endpoint)
     return results_1 == results_2
 
 def add_to_subquery(subquery: QueryTree, edge: Edge) -> QueryTree:
@@ -135,6 +157,7 @@ def add_to_subquery(subquery: QueryTree, edge: Edge) -> QueryTree:
 def merge_subquery(subqueries: set, V: set, endpoint: SPARQLWrapper) -> set:
     copy_subqueries = subqueries
     merged_subqueries = set()
+    ref_merged_edges = set()
     for subquery in subqueries:
         for copy_subquery in copy_subqueries:
             common_var = subquery.get_var().intersection(copy_subquery.get_var())
@@ -142,11 +165,19 @@ def merge_subquery(subqueries: set, V: set, endpoint: SPARQLWrapper) -> set:
                 if subquery.get_relevant_source(endpoint) == copy_subquery.get_relevant_source(endpoint):
                     if not common_var.intersection(V):
                         merged_edges = subquery.edges.union(copy_subquery.edges)
-                        merged_subquery = "SELECT * \nWHERE {\n "
-                        for edge in merged_edges:
-                            merged_subquery += str(edge) + " .\n"
-                        merged_subquery += "}"
-                        merged_subqueries.add(merged_subquery)
+                        if merged_edges.intersection(ref_merged_edges):
+                            ref_merged_edges = ref_merged_edges.union(merged_edges)
+                            str_prefix = ""
+                            for prefix in subquery.prefix:
+                                str_prefix+= str(prefix) + "\n"
+                            merged_subquery = str_prefix + "SELECT * \nWHERE {\n "
+                            for edge in merged_edges:
+                                merged_subquery += str(edge) + " .\n"
+                            merged_subquery += "}"
+                            merged_subqueries.add(merged_subquery)
+                        else:
+                            merged_subqueries.add(subquery)
+                            merged_subqueries.add(copy_subquery)
                     else:
                         merged_subqueries.add(subquery)
                         merged_subqueries.add(copy_subquery)
@@ -168,13 +199,18 @@ def cli():
 
 @cli.command()
 @click.argument("query", type=click.Path(exists=True, file_okay=True, dir_okay=True))
-@click.argument("global-join-var", type=str, nargs=-1)
+@click.option("--local-join-var", type=str, multiple=True)
+@click.option("--global-join-var", type=str, multiple=True)
 @click.argument("out-result", type=click.Path(exists=False, file_okay=True, dir_okay=True))
 @click.argument("endpoint", type=str)
 @click.pass_context
-def decompose(ctx: click.Context, query, global_join_var, out_result, endpoint):
+def decompose(ctx: click.Context, query, local_join_var, global_join_var, out_result, endpoint):
     sparql = SPARQLWrapper(endpoint)
     sparql.setReturnFormat(JSON)
+    LJV = set(Node(local_join_var_i) for local_join_var_i in local_join_var)
+    GJV = set(Node(global_join_var_i) for global_join_var_i in global_join_var)
+
+    JV = LJV.union(GJV)
 
     subqueries = set()
     query_txt = ""
@@ -187,12 +223,12 @@ def decompose(ctx: click.Context, query, global_join_var, out_result, endpoint):
     best_decomposition = set()
     min_decomp_cost = sys.maxsize
 
-    if not query_txt:
-        return Q
+    if not GJV:
+        best_decomposition.add(Q)
        
     triples = Q.get_triple_patterns()
 
-    for j_var_i in global_join_var:
+    for j_var_i in GJV:
         visited_triples = set()
         nodes = set()
         subqueries = set()
@@ -200,7 +236,9 @@ def decompose(ctx: click.Context, query, global_join_var, out_result, endpoint):
 
         while nodes:
             vrtx = nodes.pop()
+            #print(f"SELECTED NODE: {vrtx}")
             edges = Q.get_edges(vrtx)
+            #print(f"LINKED EDGES: {edges}")
 
             if not subqueries:
 
@@ -209,38 +247,58 @@ def decompose(ctx: click.Context, query, global_join_var, out_result, endpoint):
                     if edge_i in visited_triples:
                         continue
 
-                    sq = create_subquery(edge_i)
+                    sq = create_subquery(edge_i, Q.prefix)
+
                     subqueries.add(sq)
-                    nodes.add(edge_i.object)
+                    if str(edge_i.object) == str(vrtx):
+                        nodes.add(edge_i.subject)
+                    else:
+                        nodes.add(edge_i.object)
+                    #print(f"ADD NODE: {edge_i.object}")
                     visited_triples.add(edge_i)
+                    #print(f"VISITED EDGE: {edge_i}")
                 continue
 
             parent_sq = get_parent_subquery(vrtx, subqueries)
+
+            #print(f"PARENT SUBQUERY: \n\n{parent_sq}\n")
+
             for edge_i in edges:
 
                 if edge_i in visited_triples:
                     continue
 
-                if can_be_added_to_subquery(parent_sq, edge_i, global_join_var, sparql):
+                if can_be_added_to_subquery(parent_sq, edge_i, GJV, sparql):
                     parent_sq = add_to_subquery(parent_sq, edge_i)
+                    #print(parent_sq)
 
                 else:
-                    sq = create_subquery(edge_i)
+                    sq = create_subquery(edge_i, Q.prefix)
                     subqueries.add(sq)
-                nodes.add(edge_i.object)
-                visited_triples.add(edge_i)
 
-        if visited_triples == Q:
-            subqueries = merge_subquery(subqueries, global_join_var, sparql)
+                if str(edge_i.object) == str(vrtx):
+                    nodes.add(edge_i.subject)
+                else:
+                    nodes.add(edge_i.object)
+                #print(f"ADD NODE: {edge_i.object}")
+                visited_triples.add(edge_i)
+                #print(f"VISITED EDGE: {edge_i}")
+
+        #print(visited_triples == triples)
+        print(f"visited_triples: {len(visited_triples)} == triples: {len(triples)}")
+
+        if visited_triples == triples:
+            subqueries = merge_subquery(subqueries, GJV, sparql)
+            #print(subqueries)
             cost = estimate_cost(subqueries)
 
-            if cost < min_decomp_cost:
+            if cost < min_decomp_cost and cost > 0:
                 best_decomposition = subqueries
                 min_decomp_cost = cost
 
     with open(out_result, 'w') as out:
         for subquery in best_decomposition:
-            out.write(subquery + "\n")
+            out.write(str(subquery) + "\n")
 
 if __name__ == "__main__":
     cli()
